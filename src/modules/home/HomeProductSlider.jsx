1ff0581a4d4b942397cb3cb8d5be7ee388900059
Leandro Magallanes <lamagallanes@mobilecomputing.com.ar>
2025-11-27 00:18:41 -0300

    fix: repair slider

--- src/modules/home/HomeProductSlider.jsx
+++ src/modules/home/HomeProductSlider.jsx
@@ -2,7 +2,7 @@ import './HomeProductSlider.css';
 
 export default function HomeProductSlider({ children: products }) {
   return (
-    <div className="w-full min-h-[100dvh] flex overflow-x-auto hide-scrollbar slider-container">
+    <div className="w-full min-h-[100dvh] flex hide-scrollbar slider-container">
       <div className="flex flex-none slider-animation">{products}</div>
       <div aria-hidden="true" className="flex flex-none slider-animation">
         {products}
