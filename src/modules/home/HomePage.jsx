7081174d87e66689ce140690bdaf122a12eee5b7
Leandro Magallanes <patagoniascript@gmail.com>
2025-12-14 11:54:34 -0300

    feat: update homepage text and product images

--- src/modules/home/HomePage.jsx
+++ src/modules/home/HomePage.jsx
@@ -63,19 +63,19 @@ export default async function Home() {
       {/* Grid de Productos - 1 columna en mobile, 2 en desktop */}
       <div className="grid grid-cols-1 md:grid-cols-2 w-full">
         <HomeProduct
-          imageSrc="/assets/clothes.jpg"
+          imageSrc="/assets/clothes2.png"
           altText="Clothes 1"
-          title="MUSCULOSA CALADA ARENA"
-          description="Tejido perforado. Cuello henley. Frescura y estilo en su máxima expresión."
+          title="CAMISA ESSENTIAL PERFORADA"
+          description="Tejido calado de algodón. Cuello abierto tipo resort. Frescura y elegancia relajada."
           linkTitle="Descubrir Más"
           link="/products"
         />
 
         <HomeProduct
-          imageSrc="/assets/clothes2.jpg"
+          imageSrc="/assets/clothes.png"
           altText="Clothes 2"
-          title="Camisa Negra"
-          description="Algodón de alta densidad. Silueta relajada. Versatilidad absoluta."
+          title="SET ESSENTIAL TEXTURADO"
+          description="Algodón texturado de tacto suave. Silueta relajada.Versatilidad absoluta."
           linkTitle="Ver Más"
           link="/products"
         />
@@ -88,7 +88,7 @@ export default async function Home() {
           imageSrc="/assets/clothes4.jpg"
           altText="Clothes 4"
           title="SET WAFFLE ESSENTIAL"
-          description="Textura diagonal. Silueta relajada. El uniforme del verano contemporáneo."
+          description="Algodón waffle texturado. Silueta relajada. El uniforme del verano contemporáneo."
           linkTitle="Descubrir Más"
           link="/products"
         />
@@ -97,8 +97,8 @@ export default async function Home() {
           width="w-[100vw] md:w-[50vw]"
           imageSrc="/assets/clothes3.jpg"
           altText="Clothes 3"
-          title="CAMISA CALADA CREMA"
-          description="Algodón de alta densidad. Silueta relajada. Versatilidad absoluta."
+          title="MUSCULOSA RELAXED ESSENTIAL"
+          description="Algodón texturado. Silueta cómoda. Estilo minimal para uso diario."
           linkTitle="Descubrir Más"
           link="/products"
         />
@@ -107,8 +107,8 @@ export default async function Home() {
           width="w-[100vw] md:w-[50vw]"
           imageSrc="/assets/clothes5.jpg"
           altText="Clothes 5"
-          title="REMERA OVERSIZED ESSENTIAL"
-          description="Algodón de alta densidad. Silueta relajada. Versatilidad absoluta."
+          title="REMERA ESSENTIAL TEXTURADA"
+          description="Algodón texturado de tacto suave. Silueta relajada. Diseño versátil para uso diario."
           linkTitle="Ver Más"
           link="/products"
         />
